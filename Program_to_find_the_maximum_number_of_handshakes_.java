36757ea7dc46e2c83dd0b48ddae80f1e5c43cc43
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-02-25 21:01:14 +0530

    import java.util.*; public class Div{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         //double b=sc.nextInt();         int res=a*(a-1)/2;         System.out.println(res);     }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        //double b=sc.nextInt();
+        int res=a*(a-1)/2;
+        System.out.println(res);
+    }
+    
+}
