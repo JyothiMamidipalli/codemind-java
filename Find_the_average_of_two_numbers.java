0812fec80749b677ae624b58467133e180fcb622
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-02-25 20:56:12 +0530

    import java.util.*; public class Jyo{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double b=sc.nextInt();         double res=(a+b)/2;         System.out.printf("%.4f",res);     }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Jyo{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        double res=(a+b)/2;
+        System.out.printf("%.4f",res);
+    }
+    
+}
