f541a6aa4be9af576172b48b287a5281c245cb80
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-02-25 20:50:37 +0530

    import java.util.*; public class Jyothi{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         System.out.printf("%.2f",3.14*a*a);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Jyothi{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.printf("%.2f",3.14*a*a);
+    }
+}
