d1e19de3685ffb73b11efcb10a40fcbf621edaec
Jyothi Swaroopa Mamidipalli <119839745+JyothiMamidipalli@users.noreply.github.com>
2024-02-25 20:58:59 +0530

    import java.util.*; public class Div{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextFloat();         double b=sc.nextFloat();         double res=a*b;         System.out.printf("%.2f",res);     }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextFloat();
+        double b=sc.nextFloat();
+        double res=a*b;
+        System.out.printf("%.2f",res);
+    }
+    
+}
